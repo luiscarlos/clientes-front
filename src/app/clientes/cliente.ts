99e8d53bace7a20235c1640bc8555d0b0704cc89
luiscarlos <luiscarlos8pe@gmail.com>
2024-03-20 20:01:24 -0300

    Criando formulario de cliente

---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,7 @@
+export class Cliente{
+id: any;
+nome: any;
+cpf: any;
+dataCadastro: any;
+
+}
--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Cliente } from '../cliente'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './clientes-form.component.html',
   styleUrl: './clientes-form.component.css'
 })
-export class ClientesFormComponent {
+export class ClientesFormComponent implements OnInit{
 
+  cliente: Cliente;
+
+  constructor() {
+    this.cliente = new Cliente();
+    this.cliente.nome='luis';
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  clicar(){
+    console.log(this.cliente);
+  }
 }
